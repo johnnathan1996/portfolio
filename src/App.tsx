cb55d88b5dbffaa1f001380000ee4206f4e79a38
john-nathan Michielsens <john-nathan.michielsens@student.ehb.be>
2020-08-21 14:27:44 +0200

    config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,38 @@ import { Works } from './components/works';
 import { Contact } from './components/contact';
 import './global.css'
 
+import {ThemeProvider} from 'styled-components'
+
+const theme = {
+  flexboxgrid: {
+    // Defaults
+    gridSize: 12, // columns
+    gutterWidth: 0, // rem
+    outerMargin: 2, // rem
+    mediaQuery: 'only screen',
+    container: {
+      sm: 46, // rem
+      md: 61, // rem
+      lg: 76  // rem
+    },
+    breakpoints: {
+      xs: 0,  // em
+      sm: 48, // em
+      md: 64, // em
+      lg: 75  // em
+    }
+  }
+}
+
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
     <Header />
     <About />
     <Works />
     <Contact />
-    </>
+    </ThemeProvider>
   );
 }
 
