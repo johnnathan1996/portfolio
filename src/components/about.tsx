e73040edf13f6fde3a829ca7810916e7050e27f2
john-nathan Michielsens <john-nathan.michielsens@student.ehb.be>
2020-08-21 13:43:10 +0200

    design about me

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,19 +1,62 @@
 import React from "react";
 import styled from 'styled-components';
 import { Col, Row } from 'react-styled-flexboxgrid'
+import { Parallax } from 'react-parallax';
 
-const HeaderStyle = styled.div`
-  background: blue;
-  height: 100vh;
+const Container = styled.div`
+    height: 80vh;
+    padding: 10vh 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+const DisplayText = styled(Col)`
+    line-height: 30px;
+    color: rgb(100,100,100);
+    font-family: 'Montserrat';
+    font-weight: 200;
+    font-style: italic;
+`;
+
+
+const DisplayImage = styled(Col)`
+    height: 100%;
+`;
+
+
+const ParallaxImg = styled(Parallax)`
+overflow: visible !important;
 `;
 
 
 export const About = () => {
-    return <HeaderStyle>
-        <h1>
-            About
-        </h1>
+    return <Container>
 
 
-    </HeaderStyle>
+
+        <DisplayText xs={5} >
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            </DisplayText>
+
+
+        <DisplayImage xs={5} >
+
+
+            <ParallaxImg
+                bgImage={require("../images/aboutMe.png")}
+                strength={-50}
+                bgImageStyle={{
+                    width: '90%',
+                }} />
+
+
+        </DisplayImage>
+
+
+    </Container >
 }
