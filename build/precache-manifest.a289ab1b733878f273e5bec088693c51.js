d477108f982d2949a739c10077c70b9682f9bcf8
john-nathan Michielsens <john-nathan.michielsens@student.ehb.be>
2020-08-26 22:28:21 +0200

    add build folder

---
+++ build/precache-manifest.a289ab1b733878f273e5bec088693c51.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fda6c0022942a2d4671107b0649d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d24c1dc99d59706ec9",
+    "url": "/static/css/main.d2fc0e6e.chunk.css"
+  },
+  {
+    "revision": "869bef6d08dd586c8416",
+    "url": "/static/js/2.f862816c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f862816c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d24c1dc99d59706ec9",
+    "url": "/static/js/main.727f6872.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ef618a6aab0f0b9a70edb164534bb7d9",
+    "url": "/static/media/aboutMe.ef618a6a.png"
+  },
+  {
+    "revision": "6fe1bb060565ed7ebf80d3eb5a7a4582",
+    "url": "/static/media/banner.6fe1bb06.jpg"
+  },
+  {
+    "revision": "5589ccee62adc1e516165b78967f9637",
+    "url": "/static/media/facebook-logo.5589ccee.svg"
+  },
+  {
+    "revision": "376bb23c290815bc2fd4c0c59701e285",
+    "url": "/static/media/github-logo.376bb23c.svg"
+  },
+  {
+    "revision": "230582dc9a7d5c250697fb3d838fb00c",
+    "url": "/static/media/gluon_logo.230582dc.png"
+  },
+  {
+    "revision": "96d5e449c41ecc282515f40baf7c403f",
+    "url": "/static/media/insta.96d5e449.svg"
+  },
+  {
+    "revision": "18c1430ac6c85dcd7fdcd6d8e35989d1",
+    "url": "/static/media/lbn_logo.18c1430a.png"
+  },
+  {
+    "revision": "e7e232eb9fa53731cf3ccb8d05458c39",
+    "url": "/static/media/linkedin-sign.e7e232eb.svg"
+  },
+  {
+    "revision": "a3c0b72c562b27b7e927f1b790fbd059",
+    "url": "/static/media/sobema_logo.a3c0b72c.png"
+  },
+  {
+    "revision": "6aacf447eb0398cecf631c49c9b8252a",
+    "url": "/static/media/spielberg_logo.6aacf447.png"
+  }
+]);
